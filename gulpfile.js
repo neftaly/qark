042a4ad2b125ff3cef51801d635032ee35116fe2
Neftaly Hernandez <neftaly.hernandez@gmail.com>
2015-04-01 10:20:30 +1300

    Add Gulp build script

--- gulpfile.js
+++ gulpfile.js
@@ -2,9 +2,27 @@
 
 
 var gulp = require("gulp"),
-    eslint = require("gulp-eslint");
+    gutil = require("gulp-util"),
+    eslint = require("gulp-eslint"),
+
+    sourcemaps = require("gulp-sourcemaps"),
+    concat = require("gulp-concat"),
+    minifyCss = require("gulp-minify-css"),
+    browserify = require("browserify"),
+    collapse = require("bundle-collapser/plugin"),
+    source = require("vinyl-source-stream"),
+    buffer = require("vinyl-buffer"),
+    uglify = require("gulp-uglify");
+
+var bundler;
+
+
+/**
+ * Main tasks
+ */
 
 gulp.task("default", ["lint"]);
+gulp.task("build", ["html", "scss", "js"]);
 
 
 /**
@@ -23,3 +41,61 @@ gulp.task("lint", function () {
         .pipe(eslint.format());
         //.pipe(eslint.failOnError());
 });
+
+
+/**
+ * Build HTML
+ */
+
+gulp.task("html", function () {
+    return gulp.src("./src/resources/index.html")
+        .pipe(gulp.dest("./dist"));
+});
+
+
+/**
+ * Build CSS
+ */
+
+gulp.task("scss", function () {
+    //return gulp.src("./src/**/*.scss")
+    return gulp.src("./src/**/*.css")
+        .pipe(sourcemaps.init())
+        //.pipe(sass())
+        .pipe(minifyCss({
+            keepSpecialComments: 0
+        }))
+        .pipe(concat("app.min.css"))
+        .pipe(sourcemaps.write("."))
+        .pipe(gulp.dest("./dist"));
+});
+
+
+/**
+ * Build Browserified JS
+ */
+
+gulp.task("js", bundleJs);
+
+function bundleJs () {
+    return bundler.bundle()
+        .on("error", gutil.log.bind(gutil, "Browserify Error"))
+        .pipe(source("app.min.js"))
+        .pipe(buffer())
+        .pipe(sourcemaps.init({ loadMaps: true }))
+        .pipe(uglify())
+        .pipe(sourcemaps.write("./"))
+        .pipe(gulp.dest("./dist"));
+}
+
+bundler = browserify(["./src/index.js"], {
+    fullPaths: false,
+    extensions: [
+        ".js",
+        ".jsx"
+    ]
+})
+    .transform("babelify")
+    .transform("brfs")
+    .plugin(collapse)
+    .on("log", gutil.log);
